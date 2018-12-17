7ddd65683c6b00fe5896a73523f4d4e820e75af7
Lenny Merlo <lenny.merlo@gmail.com>
2018-12-17 10:37:00 +0100

    Added sum calculation in App and Calculator class to calculate sum

--- src/main/java/ch/bbw/ml/ConsoleCalculatorWithMaven/App.java
+++ src/main/java/ch/bbw/ml/ConsoleCalculatorWithMaven/App.java
@@ -1,13 +1,25 @@
 package ch.bbw.ml.ConsoleCalculatorWithMaven;
 
 /**
- * Hello world!
- *
+ * App
+ * @author Lenny
+ * @version 0.0.1
  */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Calculator calculator = new Calculator();
+        int valueA = 0;
+        int valueB = 0;
+
+        System.out.println("Console Calculator");
+        System.out.println("==================");
+        System.out.println();
+
+        valueA = 10;
+        valueB = 20;
+        System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.summe(valueA, valueB));
     }
 }
---
+++ src/main/java/ch/bbw/ml/ConsoleCalculatorWithMaven/Calculator.java
@@ -0,0 +1,14 @@
+package ch.bbw.ml.ConsoleCalculatorWithMaven;
+
+/**
+ * Calculator
+ * @author Lenny
+ * @version 0.0.1
+ */
+
+public class Calculator {
+    public int summe(int summand1, int summand2)
+    {
+        return summand1 + summand2;
+    }
+}
